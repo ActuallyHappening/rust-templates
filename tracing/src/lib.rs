58e73acd1ba286e3d64db821efa56d103c221700
Caleb Yates <calebyates42@gmail.com>
2025-08-21 15:39:57 +1000

    todo

--- tracing/src/lib.rs
+++ tracing/src/lib.rs
@@ -1,15 +1,30 @@
-pub fn init_debug_tools() {
-    #[cfg(not(target_arch = "wasm32"))]
-    tracing_subscriber::fmt::init();
+pub fn init_debug_tools(filter: &str) {
+	#[cfg(not(target_arch = "wasm32"))]
+	{
+		use tracing_subscriber::fmt::{self};
+		use tracing_subscriber::EnvFilter;
+		let fmt_layer = fmt::Layer::default().with_target(true);
+		let filter_layer = EnvFilter::try_from_default_env()
+			.or_else(|_| EnvFilter::try_new(filter))
+			.unwrap();
 
-    #[cfg(target_arch = "wasm32")]
-    {
-        use tracing_subscriber::prelude::*;
-        console_error_panic_hook::set_once();
-        tracing_subscriber::registry::Registry::default()
-            .with(tracing_wasm::WASMLayer::new(
-                tracing_wasm::WASMLayerConfig::default(),
-            ))
-            .init();
-    }
+		tracing_subscriber::Registry::default()
+			.with(filter_layer)
+			.with(fmt_layer)
+			.with(ErrorLayer::default())
+			.init();
+
+		color_eyre::install()?;
+	}
+
+	#[cfg(target_arch = "wasm32")]
+	{
+		use tracing_subscriber::prelude::*;
+		console_error_panic_hook::set_once();
+		tracing_subscriber::registry::Registry::default()
+			.with(tracing_wasm::WASMLayer::new(
+				tracing_wasm::WASMLayerConfig::default(),
+			))
+			.init();
+	}
 }
